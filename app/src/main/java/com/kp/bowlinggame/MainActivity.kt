cdf92aa8d0386e675fb301937fecab520c396acf
karthickpandiyan <pandiyan289@gmail.com>
2020-01-06 18:19:26 +0530

    Initial commit of project setup.

---
+++ app/src/main/java/com/kp/bowlinggame/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kp.bowlinggame
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
