fb771d8c585bad725f124a0abf599e7a3bacfc05
karthickpandiyan <pandiyan289@gmail.com>
2020-01-06 21:17:41 +0530

    Created testAllOnes() test function to check score is returning 20.

--- app/src/test/java/com/kp/bowlinggame/BowlingGameTest.kt
+++ app/src/test/java/com/kp/bowlinggame/BowlingGameTest.kt
@@ -1,5 +1,6 @@
 package com.kp.bowlinggame
 
+import junit.framework.TestCase.assertEquals
 import org.junit.Assert
 import org.junit.Test
 import java.lang.Exception
@@ -14,5 +15,14 @@ class BowlingGameTest {
             bowlingGame.roll(0)
         }
         Assert.assertEquals(0, bowlingGame.score())
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testAllOnes() {
+        val bowlingGame = BowlingGame()
+        for (i in 0..19)
+            bowlingGame.roll(1)
+        assertEquals(20, bowlingGame.score())
     }
 }
