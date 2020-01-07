15c403016baea48d49c7125d8d211d09729df3ed
karthickpandiyan <pandiyan289@gmail.com>
2020-01-07 08:26:51 +0530

    Design is wrong.  Responsibilities are misplaced. So modifying it.

--- app/src/main/java/com/kp/bowlinggame/BowlingGame.kt
+++ app/src/main/java/com/kp/bowlinggame/BowlingGame.kt
@@ -1,10 +1,17 @@
 package com.kp.bowlinggame
 
+
+
 class BowlingGame {
     var score: Int = 0
+    private val rolls = IntArray(21)
+    private var currentRoll = 0
+
 
     fun roll(pins: Int) {
         score += pins
+        rolls[currentRoll++] = pins;
+
     }
 
     fun score(): Int {
